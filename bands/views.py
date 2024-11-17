0cc03ff9cf53d39fbf4021cd16ff897940782c0d
rowinf <964049+rowinf@users.noreply.github.com>
2024-11-18 12:04:10 +1300

    remove typings

--- bands/views.py
+++ bands/views.py
@@ -5,8 +5,18 @@ from bands.models import Musician
 
 
 def musicians(request):
-    data = {"musicians": Musician.objects.all().order_by("last_name")}
+    all_musicians = Musician.objects.all().order_by("last_name")
     paginator = Paginator(all_musicians, 2)
+    page_num = request.GET.get("page", 1)
+    page_num = int(page_num)
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+    data = {"musicians": page.object_list, "page": page}
+
     return render(request, "musicians.html", data)
 
 
