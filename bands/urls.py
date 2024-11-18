82c46eebc0aa3b38643f47ac03724ac2eda3f584
rowinf <964049+rowinf@users.noreply.github.com>
2024-11-18 15:08:21 +1300

    add musician and band pages

--- bands/urls.py
+++ bands/urls.py
@@ -5,5 +5,7 @@ from bands import views
 
 urlpatterns = [
     path("musician/<int:musician_id>/", views.musician, name="musician"),
-    path('musicians/', views.musicians, name="musicians"),
+    path("musicians/", views.musicians, name="musicians"),
+    path("", views.bands, name="bands"),
+    path("<int:band_id>", views.band, name="band"),
 ]
--- bands/views.py
+++ bands/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 
-from bands.models import Musician
+from bands.models import Musician, Band
 
 
 def musicians(request):
     all_musicians = Musician.objects.all().order_by("last_name")
-    paginator = Paginator(all_musicians, 2)
-    page_num = request.GET.get("page", 1)
-    page_num = int(page_num)
+    page_num = int(request.GET.get("page", 1))
+    try:
+        per_page = int(request.GET.get("per_page", 2))
+    except Exception as _:
+        per_page = 2
+
+    paginator = Paginator(all_musicians, per_page)
     if page_num < 1:
         page_num = 1
     elif page_num > paginator.num_pages:
         page_num = paginator.num_pages
 
     page = paginator.page(page_num)
-    data = {"musicians": page.object_list, "page": page}
+    data = {"musicians": page.object_list, "page": page, "paginator": paginator}
 
     return render(request, "musicians.html", data)
 
@@ -25,3 +29,31 @@ def musician(request, musician_id):
     data = {"musician": musician}
 
     return render(request, "musician.html", data)
+
+
+def bands(request):
+    bands = Band.objects.all().order_by("name")
+    page_num = int(request.GET.get("page", 1))
+    try:
+        per_page = int(request.GET.get("per_page", 2))
+    except Exception as _:
+        per_page = 2
+
+    paginator = Paginator(bands, per_page)
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+    data = {"bands": page.object_list, "page": page, "paginator": paginator}
+
+    return render(request, "bands.html", data)
+
+
+def band(request, band_id):
+    band = get_object_or_404(Band, id=band_id)
+    musicians = band.musicians.all()
+    data = {"band": band, "musicians": musicians}
+
+    return render(request, "band.html", data)
