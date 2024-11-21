a0f8e628cfeece8a4a84e682133b5d7329a3c68f
rowinf <964049+rowinf@users.noreply.github.com>
2024-11-22 10:47:04 +1300

    add admin panel functionality

--- bands/models.py
+++ bands/models.py
@@ -9,21 +9,26 @@ class Musician(models.Model):
 
     def __str__(self):
         return f"Musician(id={self.id}, last_name={self.last_name})"
-
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
 class Venue(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return f"Venue(id={self.id}, name={self.name})"
-
+    class Meta:
+        ordering = ['name']
 
 class Room(models.Model):
     name = models.CharField(max_length=20)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Room(id={self.id}, name={self.name})" 
+        return f"Room(id={self.id}, name={self.name})"
+    class Meta:
+        unique_together = ("venue", "name")
+        ordering = ["name"]
 
 
 class Band(models.Model):
@@ -32,3 +37,5 @@ class Band(models.Model):
 
     def __str__(self):
         return f"Band(id={self.id}, name={self.name})"
+    class Meta:
+        ordering = ["name"]
