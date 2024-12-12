161f6b06ad74697cd94160373217a0448a3fe06a
rowinf <964049+rowinf@users.noreply.github.com>
2024-12-12 23:10:17 +1300

    add basic productivity functionality

--- productivity/forms.py
+++ productivity/forms.py
@@ -13,3 +13,15 @@ class FeelingReportForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(FeelingReportForm, self).__init__(*args, **kwargs)
         self.fields["feeling"].label = ""
+
+class JournalNoteForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        "rows": "3",
+        "cols": "50",
+    }))
+
+class JournalTodoForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        "rows": "3",
+    }))
+    done = forms.BooleanField(required=False)
--- productivity/urls.py
+++ productivity/urls.py
@@ -3,5 +3,7 @@ from productivity import views
 
 urlpatterns = [
     path("", views.productivity, name="productivity"),
-    path("feeling_reports/<int:feeling_report_id>", views.edit_feeling_report, name="edit_feeling_report")
+    path("feeling_reports/<int:feeling_report_id>", views.edit_feeling_report, name="edit_feeling_report"),
+    path("journal_notes/<int:journal_note_id>", views.edit_journal_note, name="edit_journal_note"),
+    path("journal_todos/<int:journal_todo_id>", views.edit_journal_todo, name="edit_journal_todo")
 ]
